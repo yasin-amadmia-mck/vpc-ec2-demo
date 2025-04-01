68b989249b6615fa86450e88e74eb0fce62dcf60
Yasin Amadmia <Yasin_Amadmia@external.mckinsey.com>
2025-04-01 23:35:27 +0100

    Create Private Route Table and associate it with the private subnet.

--- private_subnets.tf
+++ private_subnets.tf
@@ -9,3 +9,19 @@ resource "aws_subnet" "private" {
     Name = "${var.vpc_name}-private-subnet-${var.availability_zones[count.index]}"
   }
 }
+
+resource "aws_route_table" "private" {
+
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.vpc_name}-private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnets)
+
+  subnet_id      = element(aws_subnet.private[*].id, count.index)
+  route_table_id = aws_route_table.private.id
+}
