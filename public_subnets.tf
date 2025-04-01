639cda2e40724bd49612c1b072dec6f90c45b3ff
Yasin Amadmia <Yasin_Amadmia@external.mckinsey.com>
2025-04-01 23:31:37 +0100

    Create Public route-table Associate it with Public Subnet(s) Created earlier Add route for all traffic to go via IGW created earlier.

--- public_subnets.tf
+++ public_subnets.tf
@@ -10,3 +10,25 @@ resource "aws_subnet" "public" {
   }
 }
 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.vpc_name}-public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnets)
+
+  subnet_id      = element(aws_subnet.public[*].id, count.index)
+  route_table_id = aws_route_table.public.id
+  
+}
+
+resource "aws_route" "public_internet_gateway" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+
+}
